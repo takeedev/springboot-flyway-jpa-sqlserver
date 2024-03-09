25431dd9b33d1ff6d99d33173aa6d170868809f8
peerapolkoeram <peerapolkoeram@gmail.com>
2024-03-10 00:44:16 +0700

    init project

---
+++ src/main/java/com/tpk/FlywayDatabase/FlywayDatabaseApplication.java
@@ -0,0 +1,13 @@
+package com.tpk.FlywayDatabase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlywayDatabaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlywayDatabaseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tpk/FlywayDatabase/FlywayDatabaseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tpk.FlywayDatabase;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlywayDatabaseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
